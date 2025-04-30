3632b4a9d598a43516e772e5b9b9ffff3a5d3d2d
EMMANUEL CYLDE D. FLORES <2120762@cc.usls.edu>
2025-04-30 16:33:10 +0800

    added pet class

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -1,31 +1,19 @@
 package com.ecpe308.sample;
-
 public class Pet {
-    public String name, breed; //persian, labrador, golden retriever
-    public String pet_type;  //cat/dog
+    private String name;
+    private String type;
 
-    public String getName(){
+    public String getName() {
         return name;
     }
-
-    public String getBreed(){
-        return breed;
-    }
-
-    public String getPet_type(){
-        return pet_type;
-    }
-
-    public void setName (String name){
+    public void setName(String name) {
         this.name = name;
     }
-
-    public void setBreed (String breed){
-        this.breed = breed; 
+    public String getType() {
+        return type;
     }
-    
-    public void setPet_type (String pet_type){
-        this.pet_type= pet_type;
+    public void setType(String type) {
+        this.type = type;
     }
 }
 
